b917a865c9403543f6cafcb4226d136cfd3f16e0
Stenny3 <33425247+Stenny3@users.noreply.github.com>
2018-05-17 15:54:38 -0700

    Create oneclick_weapon_defs.lua

---
+++ french flag/luarules/config/oneclick_weapon_defs.lua
@@ -0,0 +1,41 @@
+-- reloadTime is in seconds
+
+local oneClickWepDefs = {}
+
+local oneClickWepDefNames = {
+	corcrw = {
+		{ functionToCall = "ClusterBomb", reloadTime = 854, name = "Carpet Bomb", tooltip = "Drop a huge number of bombs in a circle under the Krow", weaponToReload = 3, texture = "LuaUI/Images/Commands/Bold/bomb.png",},
+	},
+	hoverdepthcharge = {
+		{ functionToCall = "ShootDepthcharge", reloadTime = 256, name = "Drop Depthcharge", tooltip = "Drops a depthchage.", weaponToReload = 1, texture = "LuaUI/Images/Commands/Bold/dgun.png",},
+	},
+	--[[
+	logkoda = {
+		{ functionToCall = "FlameTrail", reloadTime = 850, name = "Flame Trail", tooltip = "Leave a path of flame in your wake", useSpecialReloadFrame = true, texture = "LuaUI/Images/Commands/Bold/sprint.png",},
+	},
+	--]]
+	fighter = {
+		{ functionToCall = "Sprint", reloadTime = 850, name = "Speed Boost", tooltip = "Speed boost (5x for 1 second)", useSpecialReloadFrame = true, texture = "LuaUI/Images/Commands/Bold/sprint.png",},
+	},
+	--corawac = {
+	--	{ functionToCall = "Cloak", reloadTime = 600, name = "Temp Cloak", tooltip = "Cloaks for 5 seconds", useSpecialReloadFrame = true},
+	--},
+	corbtrans = {
+		{ functionToCall = "ForceDropUnit", reloadTime = 7, name = "Drop Cargo", tooltip = "Eject cargo!", useSpecialReloadFrame = true,},
+	},
+	abductor = {
+		{ functionToCall = "ForceDropUnit", reloadTime = 7, name = "Drop Cargo", tooltip = "Eject cargo!", useSpecialReloadFrame = true,},
+	},
+	corvalk = {
+		{ functionToCall = "ForceDropUnit", reloadTime = 7, name = "Drop Cargo", tooltip = "Eject cargo!", useSpecialReloadFrame = true,},
+	},		
+}
+
+
+for name, data in pairs(oneClickWepDefNames) do
+	if UnitDefNames[name] then 
+		oneClickWepDefs[UnitDefNames[name].id] = data
+	end
+end
+
+return oneClickWepDefs
